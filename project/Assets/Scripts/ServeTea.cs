bcbeb37b81499aae3096c496da82d8187669386e
Mara Dusevic <s200494@students.aie.edu.au>
2020-10-30 11:26:41 +1100

    Add (Script) : #11 : Added ServeTea script, fixed SnapToSaucer script

---
+++ project/Assets/Scripts/ServeTea.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ServeTea : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider collider)
+    {
+        if (collider.gameObject.tag == "Saucer")
+        {
+            GameObject Tea = collider.gameObject;
+
+            // WILL ADD ANIMATION HERE
+
+            Destroy(Tea, 0.5f);
+        }
+    }
+}

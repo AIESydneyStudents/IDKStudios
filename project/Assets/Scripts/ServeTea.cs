0b52884fc17d7ad4b1a480ce291d91f00a3cd45a
Mara Dusevic <s200494@students.aie.edu.au>
2020-10-30 14:50:44 +1100

    Add (Scene) : #114 : Edited prototype

--- project/Assets/Scripts/ServeTea.cs
+++ project/Assets/Scripts/ServeTea.cs
@@ -4,15 +4,38 @@ using UnityEngine;
 
 public class ServeTea : MonoBehaviour
 {
+    [SerializeField]
+    public GameObject servedUI;
+    private GameObject Teacup;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        servedUI.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Teacup == null)
+        {
+            servedUI.SetActive(false);
+        }
+    }
+
     private void OnTriggerEnter(Collider collider)
     {
         if (collider.gameObject.tag == "Saucer")
         {
-            GameObject Tea = collider.gameObject;
+            Teacup = collider.gameObject;
+
+            // TEMPORARY TEXT APPEARS TO INDICATE ACTION
+
+            servedUI.SetActive(true);
 
             // WILL ADD ANIMATION HERE
 
-            Destroy(Tea, 0.5f);
+            Destroy(Teacup, 1.0f);
         }
     }
 }

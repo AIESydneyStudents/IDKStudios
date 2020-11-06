41fcdc3a68ac4aead4e13a15c9b5001d7abefb06
Len Farag <glizzyzbro@hotmail.com>
2020-11-06 14:19:41 +1100

    Fix (Script) : #10 : Refactored code for Additives

--- project/Assets/ItemController.cs
+++ project/Assets/ItemController.cs
@@ -8,16 +8,6 @@ public class ItemController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Collective kettle = Collective.GetCollective("Kettle");
-        Collective teapot = Collective.GetCollective("Teapot");
 
-        AdditiveStack water = new AdditiveStack("Water", 5);
-        AdditiveStack heat = new AdditiveStack("Heat", 8);
-
-        kettle.InsertAdditive(ref water);
-        kettle.InsertAdditive(ref heat);
-
-        teapot.InsertAdditive(ref water);
-        teapot.MergeCollective(kettle);
     }
 }
---
+++ project/Assets/Scripts/Order Construction/Container/Container.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+public class Container : MonoBehaviour
+{
+    #region Fields
+
+    public enum Type
+    {
+        KETTLE,
+        TEAPOT,
+        CUP
+    }
+
+    [Tooltip("Which container is this?")]
+    [SerializeField]
+    public Type containerType;
+
+    protected SortedSet<Additive> additiveRepository =
+        new SortedSet<Additive>();
+
+    #endregion
+
+    #region Properties
+
+    #endregion
+
+    #region Functions
+
+    public bool ContainsAdditive(Additive additive)
+    {
+        return additiveRepository.Contains(additive);
+    }
+
+    public void InsertAdditiveToRepo(Additive additive)
+    {
+        additiveRepository.Add(additive);
+    }
+
+    public bool AdditivePrerequisitesMet(Additive additive)
+    {
+        return false;
+    }
+
+    #endregion
+}

0f46fcd3dcc47de3ab2c1dc9cf84d2fb9574a5f2
Mara Dusevic <s200494@students.aie.edu.au>
2020-11-23 02:59:56 +1100

    Add (Script) : N/A : Added script to title screen

---
+++ project/Assets/Scripts/Len/UI/PauseScreenUI.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseScreenUI : MonoBehaviour
+{
+    private static bool GameIsPaused;
+
+    public GameObject pauseMenu;
+    public GameObject pauseIcon;
+
+    private void Start()
+    {
+        GameIsPaused = false;    
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (GameIsPaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+
+    public void Pause()
+    {
+
+    }
+
+    public void Resume()
+    {
+
+    }
+
+    public void QuitToMenu()
+    {
+
+    }
+
+}
---
+++ project/Assets/Scripts/Len/UI/TitleScreenUI.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TitleScreenUI : MonoBehaviour
+{
+    [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
+
+    static void OnStartUp()
+    {
+        SceneManager.LoadScene("Main Menu", LoadSceneMode.Single);
+    }
+
+    public void ContinueGame()
+    {
+        // need to add this
+    }
+
+    public void StartNewGame()
+    {
+        SceneManager.LoadScene("Tea shop", LoadSceneMode.Single);
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
+    }
+}

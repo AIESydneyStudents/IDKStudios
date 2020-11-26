4d4f4fe5d00814813d4b7787b89f64bbce72b9dc
Mara Dusevic <s200494@students.aie.edu.au>
2020-11-27 02:28:46 +1100

    Fix (UI) : #132 : Updated attribute UI, edited pause screen

--- project/Assets/Scripts/Len/UI/PauseScreenUI.cs
+++ project/Assets/Scripts/Len/UI/PauseScreenUI.cs
@@ -5,53 +5,28 @@ using UnityEngine.SceneManagement;
 
 public class PauseScreenUI : MonoBehaviour
 {
-    private static bool GameIsPaused;
-
     public GameObject pauseMenu;
-    public GameObject pauseIcon;
 
     private void Start()
     {
-        GameIsPaused = false;
         pauseMenu.SetActive(false);
     }
 
-    private void Update()
+    public void Pause()
     {
-        if (Input.GetKeyDown(KeyCode.Escape))
-        {
-            if (GameIsPaused)
-            {
-                Resume();
-            }
-            else
-            {
-                Pause();
-            }
-        }
+        GameEventManager.Instance.TogglePauseGame();
     }
 
     // Called to continue playing game
     public void Resume()
     {
-        pauseMenu.SetActive(false);
-        Time.timeScale = 1f;
-        GameIsPaused = false;
-    }
-
-    // Called to freeze gameplay and shows menu
-    public void Pause()
-    {
-        pauseMenu.SetActive(true);
-        Time.timeScale = 0f;
-        GameIsPaused = true;
+        Pause();
     }
 
     // Goes back to the main menu
     public void QuitToMenu()
     {
         pauseMenu.SetActive(false);
-        Time.timeScale = 1f;
         SceneManager.LoadScene("Main Menu", LoadSceneMode.Single);
     }
 
